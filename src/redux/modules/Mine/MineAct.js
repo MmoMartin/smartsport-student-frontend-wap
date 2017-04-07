e7ad1cfab2052253b1feacff7ab4e8f518fbec3f
czhaoli <844657534@qq.com>
2017-04-07 15:50:58 +0800

    Update MineAct.js

--- src/redux/modules/Mine/MineAct.js
+++ src/redux/modules/Mine/MineAct.js
@@ -3,14 +3,8 @@ import {fetchList, post, get, put} from 'xunyijia-components/src/redux/publicAct
 
 export function getUserInfo() {
   return (dispatch, req) => {
-    // const succTemp = (data)=>{
-    //   console.log('succTemp', data);
-    //   const url = data.icon;
-    //   dispatch(getImgStr(url, req));
-    // };
     dispatch(get({
       path: STUDENT_SELF_INFO,
-      // succ: succTemp
     }));
   };
 }
@@ -26,6 +20,11 @@ export function getImgStr(url) {
 }
 export function changeHeadPortrait(body) {
   return (dispatch, req) => {
+    dispatch({
+      type: `${STUDENT_SELF_INFO}_GET_SUCC`,
+      data: {
+        icon: body
+    });
     dispatch(put({
       path: CHENGE_HEAD_PORTRAIT,
       body: {icon: body},
