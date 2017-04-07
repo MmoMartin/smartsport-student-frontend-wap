810b3a837f83e309038fd4b078075c3d585e22a1
czhaoli <844657534@qq.com>
2017-04-07 15:52:08 +0800

    Update MineAct.js

--- src/redux/modules/Mine/MineAct.js
+++ src/redux/modules/Mine/MineAct.js
@@ -28,7 +28,6 @@ export function changeHeadPortrait(body) {
     dispatch(put({
       path: CHENGE_HEAD_PORTRAIT,
       body: {icon: body},
-      succ: () => {dispatch(getUserInfo());}
     }));
   };
 }
